b1022c070130fdc208ef7364a980cb02b42debee
muhammadabdullahbinshariq <shariqkhursheed@gmail.com>
2026-06-27 17:07:00 +0500

    Add files via upload

---
+++ AllSubStrings.py
@@ -0,0 +1,10 @@
+def allStrings(string):
+    number = len(string) ** 2
+    start = 0
+    end = 0
+    string = "".split(string)
+    while start < len(string):
+        for i in range(0, len(string)):
+            for j in range(0, len(string)):
+                
+allStrings(0)
